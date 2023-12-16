2f3214d841c14c0c6252fde0938d4bb34a3c98b9
Arsen1 <arsflyer57@gmail.com>
2023-12-16 22:39:58 +0300

    Devs

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,7 +3,26 @@ import { Link, useParams } from "react-router-dom";
 import React from 'react';
 import Header from './../header/Header'
 import { useTranslation} from "react-i18next";
-
+const BASE_GITHUB_URL = "https://github.com/";
+const DeveloperCard = ({ name, avatarSrc }) => {
+    const githubLink = BASE_GITHUB_URL + name;
+    return (
+    <Grid item sx={{ minWidth: '150px' }}>
+      <Card sm={6} md={4}>
+        <CardMedia sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <a href={githubLink} target="_blank" rel="noopener noreferrer">
+            <Avatar alt="logo" src={avatarSrc} sx={{ width: 100, height: 100 }} />
+          </a>
+        </CardMedia>
+        <CardContent sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <a href={githubLink} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+            <Typography>{name}</Typography>
+          </a>
+        </CardContent>
+      </Card>
+    </Grid>
+    );
+ };
 const Home = (props)=>{
     const {heroData} = props;   
     const { t, i18n} = useTranslation();
@@ -70,73 +89,13 @@ const Home = (props)=>{
                 </Card>   
             </Container>
             <Container maxWidth="lg">
-                <Typography 
-                component="h4"
-                variant="h4" 
-                align="center">
+                <Typography component="h4" variant="h4" align="center">
                     {t("home.devsTitle")}
                 </Typography>
                 <Grid container direction="row" justifyContent="space-around" spacing={4}>
-                    <Grid item key={1}>
-                        <Card sm={6} md={4}>
-                            <CardMedia>
-                                <Avatar
-                                alt="logo"
-                                src="icons/logo3.png"
-                                sx={{ width: 100, height:100 }}/>
-                            </CardMedia>
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    KirillStolbov
-                                </Typography>
-                                <a href="https://github.com/KirillStolbov" target="_blank" rel="noopener noreferrer">
-                                    <Typography>
-                                        @KirillStolbov
-                                    </Typography>
-                                </a>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item key={2}>
-                        <Card sm={6} md={4}>
-                            <CardMedia display="flex" justifyContent="center">
-                                <Avatar
-                                alt="logo"
-                                src="icons/logo2.png"
-                                sx={{ width: 100, height:100 }}/>
-                            </CardMedia>
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    1Arsen1
-                                </Typography>
-                                <a href="https://github.com/1Arsen1" target="_blank" rel="noopener noreferrer">
-                                    <Typography>
-                                        @1Arsen1
-                                    </Typography>
-                                </a>
-                            </CardContent>
-                        </Card>
-                </Grid>
-                    <Grid item key={3}>
-                        <Card sm={6} md={4}>
-                            <CardMedia display="flex" justifyContent="center">
-                                <Avatar
-                                alt="logo"
-                                src="icons/logo1.png"
-                                sx={{ width: 100, height:100 }}/>
-                            </CardMedia>
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    KopVZakone
-                                </Typography>
-                                <a href="https://github.com/KopVZakone" target="_blank" rel="noopener noreferrer">
-                                    <Typography>
-                                        @KopVZakone
-                                    </Typography>
-                                </a>
-                            </CardContent>
-                        </Card>
-                    </Grid>
+                    <DeveloperCard name="KirillStolbov" avatarSrc="icons/logo3.png" />
+                    <DeveloperCard name="1Arsen1" avatarSrc="icons/logo2.png" />
+                    <DeveloperCard name="KopVZakone" avatarSrc="icons/logo1.png" />
                 </Grid>
             </Container>
         </main>
